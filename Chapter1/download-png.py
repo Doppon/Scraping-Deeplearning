4204f4d4fe9f9d4d771ea8e860b0036f51e482e8
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-12 02:22:07 +0900

    処理を分割

--- Chapter1/download-png.py
+++ Chapter1/download-png.py
@@ -1,6 +1,12 @@
 import urllib.request
 
 # サンプル画像の保存
-urllib.request.urlretrieve("http://ff-theworld.club/wp-content/uploads/2016/01/12-28.jpg", "sample.png")
+
+# urllib.request.urlretrieve("http://ff-theworld.club/wp-content/uploads/2016/01/12-28.jpg", "sample.png")
+# ↓行儀の良い書き方
+mem = urllib.request.urlopen("http://ff-theworld.club/wp-content/uploads/2016/01/12-28.jpg").read()
+
+with open("sample.png", mode="wb") as f:
+    f.write(mem)
 
 print("保存完了しました。")
