f3fd89a154581b134aadd878ef0a041155605749
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-12 01:26:31 +0900

    rubyでサンプルコードを再現

---
+++ Chapter1/download-png.rb
@@ -0,0 +1,8 @@
+require "open-uri"
+
+# サンプル画像のダウンロード
+open("http://ff-theworld.club/wp-content/uploads/2016/01/12-28.jpg") { |image|
+  File.open("sample.jpg","wb") do |file|
+    file.puts image.read
+  end
+}
