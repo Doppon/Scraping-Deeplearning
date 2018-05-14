1d42621438a074b210540f0f06e762a759af47f6
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-14 19:36:20 +0900

    ダウンロード出来る画像を選べるように

--- Chapter1/ruby/download-png.rb
+++ Chapter1/ruby/download-png.rb
@@ -1,7 +1,12 @@
 require "open-uri"
 
+puts "URLを指定してください(空白の場合FFの画像をDWします)"
+print "URL:"; url = gets.chomp
+
+url = "http://ff-theworld.club/wp-content/uploads/2016/01/12-28.jpg" if url.empty?
+
 # サンプル画像のダウンロード
-open("http://ff-theworld.club/wp-content/uploads/2016/01/12-28.jpg") { |image|
+open(url) { |image|
   File.open("sample.jpg","wb") do |file|
     file.puts image.read
   end
